c14fe29e91a6dc62e24664de1ced63884f94bb12
Dipesh Dutta <31099687+dkdutta2006@users.noreply.github.com>
2024-07-29 11:20:02 +0530

    Add project files.

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using Asp.Versioning;
+using Asp.Versioning.Routing;
+
+namespace webapitemplate
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+
+
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap = { ["apiVersion"] = typeof(ApiVersionRouteConstraint) },
+            };
+
+            config.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.DefaultApiVersion = new ApiVersion(1);
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            });
+            config.MapHttpAttributeRoutes(constraintResolver);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/v{version:apiVersion}/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using Asp.Versioning;
+
+namespace webapitemplate.Controllers
+{
+    [ApiVersion(1)]
+    [RoutePrefix("api/v{version:apiVersion}/values")]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [Route]
+        [ApiVersion(1, Deprecated = true)]
+
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", $"Api Version - {Request.GetRequestedApiVersion()?.ToString()}" };
+        }
+
+        [Route]
+        [ApiVersion(2)]
+        public IEnumerable<string> Get_v2()
+        {
+            return new[] { "value2", $"Api Version - {Request.GetRequestedApiVersion()?.ToString()}" };
+        }
+
+        // GET api/values/5
+        [Route("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [Route]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [Route("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
